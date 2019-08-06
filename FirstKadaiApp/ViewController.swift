7e76c132d972e36843909f5c7fe666ea1c5b8e24
Yuta Katayama <yuta.k.o@icloud.com>
2019-08-06 13:17:58 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Yuta Katayama on 2019/08/06.
+//  Copyright © 2019 Swift-Biginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
